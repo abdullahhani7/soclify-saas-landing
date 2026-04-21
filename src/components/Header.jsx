f9d82fc5f2286cda1cbcb6fd38f1c0ae41743701
Abdullah <abdulhhany69@gmail.com>
2026-04-21 15:42:40 +0200

    feat: add lucide icons and enhance Header component; update Hero component with background image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { ShieldCheck } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav className="fixed top-6 left-0 right-0 z-50 flex justify-center px-4">
+      <div className="flex items-center justify-between w-full max-w-5xl px-4 py-2 bg-[#161616]/80 backdrop-blur-lg border border-white/10 rounded-xl  ">
+        {/* Logo */}
+        <div className="flex items-center gap-3 group cursor-pointer">
+          <div className="w-9 h-9 bg-emerald-600 rounded-xl flex items-center justify-center shadow-lg group-hover:bg-emerald-500 transition-colors">
+            <ShieldCheck />
+          </div>
 
-export default Header
+          <span className="text-white font-bold text-lg tracking-tight">
+            SOClify
+          </span>
+        </div>
+
+        {/* Links */}
+        <div className="hidden md:flex items-center gap-6">
+          {["Home", "Services", "Pricing", "Company", "Blog"].map((item) => (
+            <Link
+              key={item}
+              href={`/${item.toLowerCase()}`}
+              className="text-gray-400 hover:text-white text-sm font-medium transition-all"
+            >
+              {item}
+            </Link>
+          ))}
+        </div>
+
+        {/* Action Button */}
+        <button className="bg-linear-to-b from-white/10 to-white/5 hover:from-white/20 hover:to-white/10 text-white px-5 py-2 rounded-md border border-white/10 text-sm font-medium transition-all shadow-xl active:scale-95">
+          Try it Now
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div>
+      <div className="absolute inset-0 bg-[url('/bg.jpg')] bg-cover bg-center opacity-30"></div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
