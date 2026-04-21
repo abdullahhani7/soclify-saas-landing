9323297acd716037164a2e2f972773204faff835
Abdullah <abdulhhany69@gmail.com>
2026-04-21 21:05:00 +0200

    feat: add SecuritySection to Home component and update Header styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import SecuritySection from "@/components/SecuritySection";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <Header />
       <Hero />
+      <SecuritySection />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import React from "react";
 
 const Header = () => {
   return (
-    <nav className="fixed top-6 left-0 right-0 z-50 flex justify-center px-4">
-      <div className="flex items-center justify-between w-full max-w-5xl px-4 py-2 bg-[#161616]/80 backdrop-blur-lg border border-white/10 rounded-xl  ">
+    <nav className="fixed top-8 left-0 right-0 z-50 flex justify-center px-4">
+      <div className="flex items-center justify-between w-full max-w-5xl px-2 py-2 bg-[#161616]/80 backdrop-blur-lg border border-white/10 rounded-lg  ">
         {/* Logo */}
         <div className="flex items-center gap-3 group cursor-pointer">
-          <div className="w-9 h-9 bg-emerald-600 rounded-xl flex items-center justify-center shadow-lg group-hover:bg-emerald-500 transition-colors">
+          <div className="w-8 h-8 bg-linear-to-bl from-emerald-500 to-emerald-900 rounded-lg flex items-center justify-center shadow-lg   transition-all">
             <ShieldCheck />
           </div>
 
@@ -23,7 +23,7 @@ const Header = () => {
             <Link
               key={item}
               href={`/${item.toLowerCase()}`}
-              className="text-gray-400 hover:text-white text-sm font-medium transition-all"
+              className="text-white  hover:text-[#22ae74] text-sm font-medium transition-all"
             >
               {item}
             </Link>
@@ -31,7 +31,7 @@ const Header = () => {
         </div>
 
         {/* Action Button */}
-        <button className="bg-linear-to-b from-white/10 to-white/5 hover:from-white/20 hover:to-white/10 text-white px-5 py-2 rounded-md border border-white/10 text-sm font-medium transition-all shadow-xl active:scale-95">
+        <button className="bg-linear-to-b from-white/19 to-white/1  hover:from-emerald-500  hover:to-emerald-900 text-white px-5 py-1.5 rounded-lg border border-white/10 text-sm font-medium transition-all   active:scale-95 cursor-pointer transition-colors  duration-300">
           Try it Now
         </button>
       </div>
