03a4bc719615a209e6e186073b61b38f00aeb5d4
Abdullah <abdulhhany69@gmail.com>
2026-04-22 10:20:35 +0200

    feat: replace Geist font with Outfit font in layout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const outfit = Outfit({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -18,10 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en" className={`${outfit.variable}   h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
